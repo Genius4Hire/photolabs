093f99be5d347f8f4f9e1a053a3597af508ca013
Genius4Hire <jordan.dennis@gmail.com>
2023-07-15 17:08:36 -0700

    Switched data sorces from default.props to data from 'mocks' folder.

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -1,19 +1,22 @@
 import React from 'react';
+import {useState} from 'react';
 
 import PhotoList from '../components/PhotoList';
 import TopNavigation from '../components/TopNavigationBar';
+import photos from "../mocks/photos";
+import topics from "../mocks/topics";
+
 
 import '../styles/HomeRoute.scss';
 
-
 const HomeRoute = () => {
+  const [mockPhotos, setMockPhotos] = useState(photos);
+  const [mockTopics, setMockTopics] = useState(topics);
+
   return (
     <div>
-      < TopNavigation />
-      < PhotoList />
-      < PhotoList />
-      < PhotoList />
-      < PhotoList />
+      < TopNavigation mockTopics={mockTopics}/>
+      < PhotoList mockPhotos={mockPhotos}/>
     </div>
   );
 };
